cd4a75bc9551a476cb975134cfa116347d867d6c
Naimul Islam <101422148+naimul-123@users.noreply.github.com>
2025-06-25 01:56:44 +0600

    ana ganda

--- components/Forms/LandForm.jsx
+++ components/Forms/LandForm.jsx
@@ -1,29 +1,99 @@
 'use client';
-import { useState } from 'react';
+import { The_Nautigal } from 'next/font/google';
+import { useEffect, useState } from 'react';
+import HissaCalculator from '../HissaCalculator';
 
 export default function LandForm() {
-    const [landName, setLandName] = useState('');
-    const [landArea, setLandArea] = useState('');
-    const [owner, setOwner] = useState('');
-    const [heirs, setHeirs] = useState([{ name: '', relation: '' }]);
+    const data = {
+        "district": "",
+        "thana": "",
+        "touji_No": "",
+        "mouja": "",
+        "j.l_no": "",
+        "khatian_No": "",
+        "khatian_type": "",
+        "plots": [],
+        "owners": [],
+        "totalLand": 0
+    }
+    const [landInfo, setLandInfo] = useState(data)
 
-    const handleHeirChange = (index, field, value) => {
-        const newHeirs = [...heirs];
-        newHeirs[index][field] = value;
-        setHeirs(newHeirs);
+    const [ownersTotalShare, setOwnersTotalShare] = useState(0)
+    useEffect(() => {
+        const totalShare = landInfo?.owners?.reduce((totalShare, owner) => totalShare + Number(owner.share) || 0, 0)
+        setOwnersTotalShare(totalShare)
+    }, [landInfo.owners]);
+    const handleOwnerChange = (index, field, value) => {
+        if (field === "share" && Number(value) + Number(ownersTotalShare) > 1) {
+            alert('মালিকানার মোট হিস্যা 1  এর বেশি হতে পারবে না।')
+            return
+        }
+        const owners = [...landInfo?.owners]; // create a shallow copy of the owners array
+        owners[index] = {
+            ...owners[index],
+            [field]: value, // update the specific field (name or share)
+        };
+        setLandInfo(prev => ({
+            ...prev,
+            owners // update only the owners array
+        }));
+
+    };
+    const handlePlotChange = (index, field, value) => {
+        if (field === "share" && value > 1) {
+            alert('দাগের হিস্যা 1  এর বেশি হতে পারবে না।')
+            return
+        }
+        const plots = [...landInfo?.plots]; // create a shallow copy of the owners array
+        plots[index] = {
+            ...plots[index],
+            [field]: value, // update the specific field (name or share)
+        };
+
+        const totalLand = plots.reduce((total, plot) => total + (plot.totalLandInPlot * plot.share), 0)
+
+        setLandInfo(prev => ({
+            ...prev,
+            plots,
+            totalLand
+            // update only the owners array
+        }));
     };
 
-    const addHeir = () => {
-        setHeirs([...heirs, { name: '', relation: '' }]);
+    const addOwner = () => {
+        const updatedowners = [...landInfo?.owners || [], {
+            "name": "",
+            "share": 0,
+        }]
+        setLandInfo({ ...landInfo, owners: updatedowners })
+    };
+    const addPlot = () => {
+        const updatedPlots = [...landInfo?.plots || [], {
+            "plot_no": "",
+            "totalLandInPlot": 0,
+            "share": 0,
+        }]
+        setLandInfo({ ...landInfo, plots: updatedPlots })
     };
 
+    const removePlot = (index) => {
+        const updatedPlots = landInfo?.plots?.filter((_, i) => i !== index);
+        const totalLand = updatedPlots.reduce((total, plot) => total + (plot.totalLandInPlot * plot.share), 0)
+
+        setLandInfo(prev => ({
+            ...prev,
+            plots: updatedPlots,
+            totalLand
+            // update only the owners array
+        }));
+
+    }
+
+    console.log(landInfo);
     const handleSubmit = (e) => {
         e.preventDefault();
         const data = {
-            landName,
-            landArea: parseFloat(landArea),
-            owner,
-            heirs
+
         };
         console.log('Submitted:', data);
         // এখানে API call যাবে
@@ -36,24 +106,23 @@ export default function LandForm() {
                 <input
                     type="text"
                     placeholder="জিলা"
-                    value={landName}
-                    onChange={(e) => setLandName(e.target.value)}
+                    defaultValue={landInfo?.district}
+
                     className="input"
                     required
                 />
                 <input
                     type="text"
                     placeholder="থানা"
-                    value={landName}
-                    onChange={(e) => setLandName(e.target.value)}
+                    defaultValue={landInfo.thana}
+
                     className="input"
                     required
                 />
                 <input
                     type="text"
                     placeholder="তৌজি"
-                    value={landName}
-                    onChange={(e) => setLandName(e.target.value)}
+                    defaultValue={landInfo.touji_No}
                     className="input"
                     required
                 />
@@ -61,66 +130,169 @@ export default function LandForm() {
                 <input
                     type="text"
                     placeholder="মৌজা"
-                    value={landName}
-                    onChange={(e) => setLandName(e.target.value)}
+                    defaultValue={landInfo?.mouja}
+
                     className="input"
                     required
                 />
                 <input
                     type="text"
                     placeholder="খতিয়ান নং"
-                    value={landName}
-                    onChange={(e) => setLandName(e.target.value)}
+                    defaultValue={landInfo?.khatian_No}
+
                     className="input"
                     required
                 />
                 <input
                     type="text"
                     placeholder="জমির পরিমাণ"
-                    value={landArea}
-                    onChange={(e) => setLandArea(e.target.value)}
+                    defaultValue={landInfo?.totalLand}
                     className="input"
                     required
                 />
 
             </div>
 
-            <div>
-                <h2 className="text-xl font-bold mb-4">মালিকগণের তথ্য</h2>
-                {heirs.map((heir, index) => (
-                    <div key={index} className="flex gap-2 mb-2">
-                        <input
-                            type="text"
-                            placeholder="নাম"
-                            value={heir.name}
-                            onChange={(e) => handleHeirChange(index, 'name', e.target.value)}
-                            className="flex-1 border p-2 rounded"
-                            required
-                        />
-                        <input
-                            type="text"
-                            placeholder="অত্র খতিয়ানে মালিকের অংশ"
-                            value={heir.relation}
-                            onChange={(e) => handleHeirChange(index, 'relation', e.target.value)}
-                            className="flex-1 border p-2 rounded"
-                            required
-                        />
-                        <div className='w-full flex-1 input'>
-                            <output className="text-green-500">
-                                {heir.name}
+            <div tabIndex={0} className='collapse collapse-arrow bg-base-100 border border-base-300'>
+                <input type="checkbox" />
+
+                <div className="collapse-title font-semibold flex justify-between">
+                    <h2>
+                        দাগের তথ্য
+                    </h2>
+                    <h2>
+                        অন্তর্ভূক্ত দাগসমূহ:  {landInfo?.plots?.map(plot => plot.plot_no).join(',')}
+                    </h2>
+                    <h2>
+                        মোট জমি:  {landInfo?.totalLand}
+                    </h2>
+                </div>
+                <div className='collapse-content text-sm overflow-x-auto max-h-96'>
+                    <table className='table table-pin-rows '>
+                        <thead>
+                            <tr className='text-wrap'>
+                                <th>প্লট নং</th>
+                                <th>দাগের মোট পরিমান</th>
+                                <th>দাগের মধ্যে অত্র স্বত্বের অংশ</th>
+                                <th className='flex flex-wrap text-wrap'>দাগের মধ্যে অত্র স্বত্বের অংশের জমির পরিমান</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {landInfo?.plots?.map((plot, index) => (
+                                <tr key={index} className="">
+                                    <td>
+                                        <input
+                                            type="text"
+                                            placeholder="প্লট নং"
+                                            value={plot.plot_no}
+                                            onChange={(e) => handlePlotChange(index, 'plot_no', e.target.value)}
+                                            className="flex-1 border p-2 rounded"
+                                            required
+                                        />
+                                    </td>
+                                    <td>
+                                        <input
+                                            type="text"
+                                            placeholder="দাগে মোট জমি"
+                                            value={plot.totalLandInPlot}
+                                            onChange={(e) => handlePlotChange(index, 'totalLandInPlot', e.target.value)}
+                                            className="flex-1 border p-2 rounded"
+                                            required
+                                        />
+                                    </td>
+                                    <td>
+                                        <HissaCalculator />
+                                        <input
+                                            type="text"
+                                            placeholder="হিস্যা"
+                                            value={plot.share}
+                                            onChange={(e) => handlePlotChange(index, 'share', e.target.value)}
+                                            className="flex-1 border p-2 rounded"
+                                            required
+                                        />
+                                    </td>
+                                    <td>
+                                        <output className="text-green-500 flex-1 input">
+                                            {landInfo?.plots[index].share * landInfo?.plots[index].totalLandInPlot || 0}
+                                        </output>
+
+                                    </td>
+                                    <td><span className='btn btn-error btn-soft' onClick={() => removePlot(index)}>-</span></td>
+
+
+
+
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+
+                    <span
+                        type="button"
+                        onClick={addPlot}
+                        className="text-sm btn btn-link text-blue-600"
+                    >
+                        + নতুন দাগ
+                    </span>
+                </div>
+
+
+            </div>
+            <div tabIndex={0} className='collapse collapse-arrow bg-base-100 border border-base-300'>
+                <input type="checkbox" />
+
+                <div className="collapse-title font-semibold flex justify-between">
+                    <h2>
+                        মালিকগণের তথ্য
+                    </h2>
+                    <h2>
+                        মোট মালিক:  {landInfo?.owners?.length}
+                    </h2>
+                    <h2>
+                        অবশিষ্ট মালিকানা অংশ:  {1 - ownersTotalShare}
+                    </h2>
+                    <h2>
+                        মোট জমি:  {ownersTotalShare * landInfo.totalLand}
+                    </h2>
+                </div>
+                <div className='collapse-content text-sm'>
+                    {landInfo?.owners?.map((owner, index) => (
+                        <div key={index} className="flex gap-2 mb-2">
+                            <input
+                                type="text"
+                                placeholder="নাম"
+                                value={owner.name}
+                                onChange={(e) => handleOwnerChange(index, 'name', e.target.value)}
+                                className="flex-1 border p-2 rounded"
+                                required
+                            />
+                            <input
+                                type="text"
+                                placeholder="অত্র খতিয়ানে মালিকের অংশ"
+                                value={owner.share}
+                                onChange={(e) => handleOwnerChange(index, 'share', e.target.value)}
+                                className="flex-1 border p-2 rounded"
+                                required
+                            />
+                            <output className="text-green-500 flex-1 input">
+                                {owner.share * landInfo?.totalLand || 0}
                             </output>
 
-                        </div>
 
-                    </div>
-                ))}
-                <button
-                    type="button"
-                    onClick={addHeir}
-                    className="text-sm btn btn-link text-blue-600"
-                >
-                    + নতুন মালিক যোগ করুন
-                </button>
+                        </div>
+                    ))}
+                    <span
+
+                        disabled={ownersTotalShare >= 1}
+                        onClick={addOwner}
+                        className="text-sm btn btn-link text-blue-600"
+                    >
+                        + নতুন মালিক
+                    </span>
+                </div>
+
+
             </div>
             <button
                 type="submit"
---
+++ components/HissaCalculator.jsx
@@ -0,0 +1,84 @@
+import React from 'react'
+
+const HissaCalculator = () => {
+    return (
+        <div className='flex'>
+            <select className='select select-sm' name='ana'>
+                <option value="0">আনা সিলেক্ট করুন</option>
+                <option value="1">/ =১ আনা</option>
+                <option value="2">&#x09f5; =২ আনা</option>
+                <option value="3">&#x09f6; =৩ আনা</option>
+                <option value="4">&#x09f7; =৪ আনা</option>
+                <option value="5">&#x09f7;/ =৫ আনা</option>
+                <option value="6">&#x09f7;&#x09f5; =৬ আনা</option>
+                <option value="7">&#x09f7;&#x09f6; =৭ আনা</option>
+                <option value="8">&#x09f7;&#x09f7; =৮ আনা</option>
+                <option value="9">&#x09f7;&#x09f7;/ =৯ আনা</option>
+                <option value="10">&#x09f7;&#x09f7;&#x09f5; =১০ আনা</option>
+                <option value="11">&#x09f7;&#x09f7;&#x09f6; =১১ আনা</option>
+                <option value="12">&#x09f8; =১২ আনা</option>
+                <option value="13">&#x09f8;/ =১৩ আনা</option>
+                <option value="14">&#x09f8;&#x09f5; =১৪ আনা</option>
+                <option value="15">&#x09f8;&#x09f6; =১৫ আনা</option>
+                <option value="16">&#x09e7; =১৬ আনা</option>
+            </select>
+            <select className='select select-sm' name='ganda'>
+                <option value="0">গন্ডা সিলেক্ট করুন</option>
+                <option value="1">১ গন্ডা</option>
+                <option value="2">২ গন্ডা</option>
+                <option value="3">৩ গন্ডা</option>
+                <option value="4">৪ গন্ডা</option>
+                <option value="5">৫ গন্ডা</option>
+                <option value="6">৬ গন্ডা</option>
+                <option value="7">৭ গন্ডা</option>
+                <option value="8">৮ গন্ডা</option>
+                <option value="9">৯ গন্ডা</option>
+                <option value="10">১০ গন্ডা</option>
+                <option value="11">১১ গন্ডা</option>
+                <option value="12">১২ গন্ডা</option>
+                <option value="13">১৩ গন্ডা</option>
+                <option value="14">১৪ গন্ডা</option>
+                <option value="15">১৫ গন্ডা</option>
+                <option value="16">১৬ গন্ডা</option>
+                <option value="17">১৭ গন্ডা</option>
+                <option value="18">১৮ গন্ডা</option>
+                <option value="19">১৯ গন্ডা</option>
+            </select>
+            <select className='select select-sm' name='kara'>
+                <option value="0">কড়া সিলেক্ট করুন</option>
+                <option value="1">&#x0964; =১ কড়া</option>
+                <option value="2">&#x0965; =২ কড়া</option>
+                <option value="3">&#x09f8; =৩ কড়া</option>
+            </select>
+            <select className='select select-sm' name='kara'>
+                <option value="0">ক্রান্তি সিলেক্ট করুন</option>
+                <option value="1">&#x09f4; =১ ক্রান্তি</option>
+                <option value="2">&#x09f4;&#x09f4; =২ ক্রান্তি</option>
+            </select>
+            <select className='select select-sm' name='til'>
+                <option value="0">তিল সিলেক্ট করুন</option>
+                <option value="1">১ তিল</option>
+                <option value="2">২ তিল</option>
+                <option value="3">৩ তিল</option>
+                <option value="4">৪ তিল</option>
+                <option value="5">৫ তিল</option>
+                <option value="6">৬ তিল</option>
+                <option value="7">৭ তিল</option>
+                <option value="8">৮ তিল</option>
+                <option value="9">৯ তিল</option>
+                <option value="10">১০ তিল</option>
+                <option value="11">১১ তিল</option>
+                <option value="12">১২ তিল</option>
+                <option value="13">১৩ তিল</option>
+                <option value="14">১৪ তিল</option>
+                <option value="15">১৫ তিল</option>
+                <option value="16">১৬ তিল</option>
+                <option value="17">১৭ তিল</option>
+                <option value="18">১৮ তিল</option>
+                <option value="19">১৯ তিল</option>
+            </select>
+        </div>
+    )
+}
+
+export default HissaCalculator
