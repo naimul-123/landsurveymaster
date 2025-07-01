1d77c7266dfbaef83d6114fc913ad090b15366e7
Naimul Islam <101422148+naimul-123@users.noreply.github.com>
2025-07-01 09:27:36 +0600

    khatian details is running

--- app/viewkhatian/page.jsx
+++ app/viewkhatian/page.jsx
@@ -19,18 +19,23 @@ const ViewKhatian = () => {
   });
   console.log(khatianinfo);
   return (
-    <div className="flex gap-2">
-      <ul>
-        <h2>খতিয়ান তালিকা</h2>
+    <div className="flex gap-2 ">
+      <ul className="shadow-sm">
+        <h2 className="text-2xl font-bold text-primary p-4 border-b text-center">খতিয়ান তালিকা</h2>
         {khatianinfo?.map((khatian) => (
-          <li type="button" onClick={() => setKhatianId(khatian._id)}>
-            <span>খতিয়ান নং {khatian.khatian_No}</span>
+          <li type="button" onClick={() => setKhatianId(khatian._id)} className={`border-b hover:bg-accent-content p-4 hover:text-white ${khatian._id === khatian_id ? 'bg-accent-content text-white' : ''}`}>
+            <div className="card w-60  card-xs rounded-none ">
+              <div className="card-body">
+                <h2 className="card-title">{khatian?.owners[0].name} গং</h2>
+                <p>জেলা: {khatian?.district} থানা: {khatian.thana} মৌজা:{khatian.mouja} {khatian.khatian_type} {khatian?.khatian_No} নং খতিয়ান</p>
+              </div>
+            </div>
           </li>
         ))}
       </ul>
-      <div>
-        <KhatianDetails khatian={selectedKhatina} />
-      </div>
+
+      <KhatianDetails khatian={selectedKhatina} />
+
     </div>
   );
 };
--- components/KhatianDetails.jsx
+++ components/KhatianDetails.jsx
@@ -2,7 +2,55 @@ import React from "react";
 
 const KhatianDetails = ({ khatian }) => {
   console.log(khatian);
-  return <div>{khatian?.khatian_No}</div>;
+  return (
+    <div>
+
+
+      <table className="table table-border">
+        <thead>
+          <tr>
+            <th>মালিকগণের নাম ও ঠিকানা</th>
+            <th>অত্র খতিয়ানে মালিকের অংশ</th>
+            <th>খতিয়ানে মালিকের মোট জমি</th>
+          </tr>
+        </thead>
+        <tbody>
+          {khatian?.owners.map(owner => (
+            <>
+              <tr>
+                <td>{owner.name}</td>
+                <td>{owner.share}</td>
+                <td>{owner.share * khatian.totalLand}</td>
+              </tr>
+              <tr>
+                <td colSpan={3}>
+                  <table>
+                    <thead>
+                      <th>দাগ নং</th>
+                      <th>দাগে মোট জমি</th>
+                      <th>দাগে মালিকের জমি</th>
+                    </thead>
+                    <tbody>
+                      {khatian.plots.map(plot => (
+                        <tr>
+                          <td>{plot.plot_no
+                          }</td>
+                          <td>{plot.totalLandInPlot * plot.share}</td>
+                          <td>{plot.totalLandInPlot * plot.share * owner.share}</td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </td>
+              </tr>
+
+            </>
+          ))}
+
+        </tbody>
+      </table>
+
+    </div>);
 };
 
 export default KhatianDetails;
